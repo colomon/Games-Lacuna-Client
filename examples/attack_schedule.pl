fef58f780a2e2a20a69c180d54846db78f32c50c
Garrett Goebel <garrett.goebel@gmail.com>
2010-12-14 12:39:44 -0500

    added flight plan

--- examples/attack_schedule.pl
+++ examples/attack_schedule.pl
@@ -6,16 +6,32 @@ use 5.010;
 use List::Util            (qw(first));
 use Games::Lacuna::Client ();
 use Getopt::Long          (qw(GetOptions));
-
+use DateTime;
+use Try::Tiny;
+use YAML::Any             (qw(Dump));
 my $star;
-my $planet;
+my $orbit;
+my $eta;
 
 GetOptions(
-    'star=s'   => \$star,
-    'planet=s' => \$planet,
+    'star=s'  => \$star,
+    'orbit=s' => \$orbit,
+    'eta=s'   => \$eta,
 );
 
-usage() if !$star || !$planet;
+usage() if !$star || !$orbit || !$eta;
+
+my $t1;
+my $tminus;
+local_scope: {
+    my %eta;
+    @eta{qw(year month day hour minute second time_zone)} = (split(/:/, $eta),'UTC');
+    $t1 = DateTime->new(%eta);
+    my $t0 = DateTime->now(time_zone => 'UTC');
+    my $seconds_dur = $t1->subtract_datetime_absolute($t0);
+    $tminus = $seconds_dur->seconds();
+    say 'T minus: ', $tminus, ' seconds';
+}
 
 my $cfg_file = shift(@ARGV) || 'lacuna.yml';
 unless ( $cfg_file and -e $cfg_file ) {
@@ -29,30 +45,34 @@ my $client = Games::Lacuna::Client->new(
 
 my $empire  = $client->empire->get_status->{empire};
 my $planets = $empire->{planets};
-my $target_id;
-my $target_name;
-my $target_type;
+my %target;
+my %bol;      #Bill of Lading
 
 my $star_result = $client->map->get_star_by_name($star)->{star};
 
-# send to planet on star
-my $target_bodies = $star_result->{bodies};
-
-my ($target_body) = first { $_->{name} eq $planet } @$target_bodies;
-
-die "Planet '$planet' not found at star '$star'"
+# find planet in $orbit around $star
+my ($target_body) = grep {$_->{orbit} == $orbit} @{$star_result->{bodies}};
+die "Planet not found in orbit $orbit of star '$star'"
     if !$target_body;
 
-$target_id   = $target_body->{id};
-$target_name = "$planet [$star]";
-$target_type = "body_id";
+$target{id}   = $target_body->{id};
+$target{name} = "$target_body->{name} [$star]";
+$target{type} = 'body_id';
+$target{x}    = $target_body->{x};
+$target{y}    = $target_body->{y};
 
 for my $from_id (sort { $planets->{$a} cmp $planets->{$b} } keys %$planets) {
-    say "From " . $planets->{$from_id} . ":";
+    my %poo; # point of origin
 
     # find planet, buildings on that planet, and finally its spaceport
-    my $body         = $client->body( id => $from_id );
-    my $result       = $body->get_buildings;
+    my $body   = $client->body( id => $from_id );
+    my $result = $body->get_buildings;
+    @poo{qw(name id x y w1 w2)} = (@{$result->{status}{body}}{qw(name id x y)}); #,{}{});
+    my $planet_id = $poo{id};
+    $poo{d} = sqrt(abs($target{x}-$poo{x})**2 + abs($target{y}-$poo{y})); # distance to target from point of origin
+
+#    say "From " . $planets->{$from_id} . sprintf(" (distance: %.3f):",$poo{d});
+
     my $buildings    = $result->{buildings};
     my $space_port_id = first {
             $buildings->{$_}->{name} eq 'Space Port'
@@ -60,13 +80,33 @@ for my $from_id (sort { $planets->{$a} cmp $planets->{$b} } keys %$planets) {
     my $space_port = $client->building( id => $space_port_id, type => 'SpacePort' );
     
     # get the ships we can send from that spaceport to our target
-    my $ships = $space_port->get_ships_for( $from_id, { body_id => $target_id } );
+    my $ships = $space_port->get_ships_for( $from_id, { body_id => $target{id} } );
     my $available = $ships->{available};
 
+    # Scanners go into Wave 1, everything else into Wave 2
     for my $ship (sort { $a->{name} cmp $b->{name} } @$available) {
-        say "  " . $ship->{name};
-        my $speed = $ship->{speed};
-        
+	my ($ship_id, $name, $type, $speed) = @{$ship}{qw(id name type speed)};
+
+	my $secs = ($poo{d} / ($speed/100)) * 60 * 60; # duration in ms
+	$secs -= 60  if $ship->{type} ne 'scanner'; # arrival 60 seconds after initial eta
+	
+	my @order = (@poo{qw(id name)}, $ship_id, $name, $type, $speed);
+        my $d = DateTime::Duration->new(seconds => $secs);
+	my $launch = $t1->clone->subtract($d)->strftime('%F %T');
+
+	if ($secs < $tminus) { # if ship can arrive by eta add to a wave of attack
+	    $bol{$launch} ||= [];
+	    push @{$bol{$launch}}, [($ship->{type} eq 'scanner' ? '1' : '2'), @order];
+	}
+    }
+}
+
+print "Launch Plan $target{name}:\n";
+for my $launch (keys %bol) {
+    print "\t$launch\n";
+    for my $order (@{$bol{$launch}}) {
+	my ($wave, $planet_id, $planet_name, $ship_id, $ship_name, $ship_type, $ship_speed) = @$order;
+	print "\t\t$planet_name, $ship_name, $ship_type, $ship_speed, wave $wave\n";
     }
 }
 
@@ -75,10 +115,14 @@ for my $from_id (sort { $planets->{$a} cmp $planets->{$b} } keys %$planets) {
 sub usage {
   die <<"END_USAGE";
 Usage: $0 attack_schedule.yml
-       --star       NAME (required)
-       --planet     NAME (required)
+       --star       NAME   (required)
+       --orbit      NUMBER (required)
+       --eta        TIME   (required)
 
 Lays out the attack times to the target planet.
+
+The eta is the time at which the 1st wave should arrive.
+eta must be provided in the following format: yyyy:MM:dd HH:mm::ss
 
 END_USAGE
 
